35e89f681e004d6c198c6a8187179d593e21f811
Md Ahmed <mahmed4@dmacc.edu>
2020-11-07 14:35:23 -0600

    Lombok annotations implemented to this project

--- src/main/java/dmacc/beans/ClassInstructors.java
+++ src/main/java/dmacc/beans/ClassInstructors.java
@@ -6,7 +6,18 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ClassInstructors {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,74 +27,5 @@ public class ClassInstructors {
 	private String email;
 	@Autowired
 	private ClassItems classItems;
-	
-	public ClassInstructors() {
-		super();
-	}
-	
-	public ClassInstructors(String firstName, String lastName) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-
-	public ClassInstructors(String firstName, String lastName, String email) {
-		super();
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
-	public ClassInstructors(long id, String firstName, String lastName, String email) {
-		super();
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public ClassItems getClassItems() {
-		return classItems;
-	}
-
-	public void setClassItems(ClassItems classItems) {
-		this.classItems = classItems;
-	}
-
-	@Override
-	public String toString() {
-		return "ClassInstructors [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
-				+ ", classItems=" + classItems + "]";
-	}
-	
 
 }
--- src/main/java/dmacc/beans/ClassItems.java
+++ src/main/java/dmacc/beans/ClassItems.java
@@ -2,54 +2,21 @@ package dmacc.beans;
 
 import javax.persistence.Embeddable;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 @Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class ClassItems {
 	private String className;
 	private String preRequisite;
 	private String classType;
 	private String classSize;
-	
-	public ClassItems() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public ClassItems(String className, String preRequisite, String classType, String classSize) {
-		super();
-		this.className = className;
-		this.preRequisite = preRequisite;
-		this.classType = classType;
-		this.classSize = classSize;
-	}
-	public String getClassName() {
-		return className;
-	}
-	public void setClassName(String className) {
-		this.className = className;
-	}
-	public String getPreRequisite() {
-		return preRequisite;
-	}
-	public void setPreRequisite(String preRequisite) {
-		this.preRequisite = preRequisite;
-	}
-	public String getClassType() {
-		return classType;
-	}
-	public void setClassType(String classType) {
-		this.classType = classType;
-	}
-	public String getClassSize() {
-		return classSize;
-	}
-	public void setClassSize(String classSize) {
-		this.classSize = classSize;
-	}
-	@Override
-	public String toString() {
-		return "ClassItems [className=" + className + ", preRequisite=" + preRequisite + ", classType=" + classType
-				+ ", classSize=" + classSize + "]";
-	}
-	
-	
-
 }
